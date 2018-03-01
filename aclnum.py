f06312f13acbc6f2948c1fbf06a3f393b19c331a
Peter Bruno <brunopeter@users.noreply.github.com>
2018-03-01 11:10:04 -0500

    Update aclnum.py
    cleanup, simplification and pycodestyle
    
    Removed input and output file command line options. Default to look for one argument for input file and output to stdout -- user can then pipe to desired file or review on screen.

--- aclnum.py
+++ aclnum.py
@@ -1,61 +1,46 @@
 #!/usr/bin/python
 # Program to take a Cisco ACL and cleanup the line numbers ##
 
+from __future__ import print_function
 import sys
-import getopt
-
-x = 0
-count = 0
-acl = inputfile = outputfile = ''
 
 
 def usage(ext):
     # exit with description and command line example
     print("\nCisco ACL re-numbering utility")
-    print("usage:")
-    print("-h <help> -- this message")
-    print(("%s -i <input file> -o <output file>" % sys.argv[0]))
-    print("  note: if output is not specified, it will be sent to <stdout>")
+    print("   Renumber a Cisco ACL to increment by 10.")
+    print("\nusage:")
+    print(("\n%s <input file>" % sys.argv[0]))
     sys.exit(ext)
 
-# get command line options
-try:
-    opts, args = getopt.getopt(sys.argv[1:], 'h:i:o:')
-except getopt.GetoptError:
-    usage(2)
 
-# if no command line options given, exit
-if not opts:
-    usage(0)
+def main():
+    STEP = 10   # number to increment lines by
+    count = 0
+    acl, inputfile = '', ''
 
-# command line options parsing
-for opt, arg in opts:
-    if opt == ('-h'):
+    try:
+        inputfile = open(sys.argv[1], 'r')
+    except (IndexError, IOError):
         usage(0)
-    elif opt in ("-i", "--ifile"):
-        inputfile = open(arg, 'r')
-    elif opt in ("-o", "--ofile"):
-        outputfile = open(arg, 'w')
 
-# process input file
-for line in inputfile:
-    count += 1
-    s = line.strip().split(" ", 1)
-    if s[0].isdigit():
-        x += 10
-        acl = str(x) + " " + s[1]
-    else:
-        acl = line.strip()
-
-    if outputfile:
-        outputfile.write("%s\n" % acl)
-    else:
+    # process input file
+    for line in inputfile:
+        s = line.strip().split(" ", 1)
+        if s[0].isdigit():
+            count += 1
+            acl = " " + str(count*STEP) + " " + s[1]
+        else:
+            acl = line.strip()
         print(acl)
 
-# close files before exiting
-if inputfile:
-    inputfile.close()
-if outputfile:
-    outputfile.close()
+    # close files before exiting
+    if inputfile:
+        inputfile.close()
 
-print(("\n\n Processed %s lines." % count))
+    print(("\n\n!!Processed %s lines." % count))
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    main()
